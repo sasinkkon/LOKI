339432a050bf18572db5b3659efc76ba58f54375
xMvon <nieminenmvo@gmail.com>
2025-09-21 19:17:09 +0300

    muokattu sivuston luurankoa ja lisätty melkein toimiva kalenteri juttu

---
+++ Blazori/Calendaritem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOriented_Template
+{
+    public record CalendarItem(DateTime Date, string Name);
+}
