47ad99f2a48ec96f82d71eceb480b09d6459cf9b
Sanetsi <santtuaku@gmail.com>
2025-10-06 19:02:31 +0300

    Viimeistelty käyttäjän tarkistus ja sisäänkirjautuminen

---
+++ Blazori/Models/CredidentalsManager.cs
@@ -0,0 +1,30 @@
+﻿using SaveLoad;
+
+namespace CredidentalsManager
+{
+
+    public class CredidentalsManager()
+    {
+        
+        public static bool isUserAccess(string userName, string password)
+        {
+
+            try
+            {
+                var LoadedUserData = DataLoading.Load(userName);
+
+                    if (LoadedUserData.Pass == password)
+                    {
+                        return true; // User and pass ok
+                    }
+            } catch
+            {
+                return false; // Return false if user data not found
+            }
+            
+
+            return false; // User not found
+        }
+    }
+}
+
