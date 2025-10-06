3fad77fabae137e8d738b21dc478792fd9ab8e1f
Sanetsi <santtuaku@gmail.com>
2025-10-06 21:31:27 +0300

    Added StorageLoading program for easy pulling of user STORAGE data onto page git status

---
+++ Blazori/Models/StorageLoading.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace SaveLoad
+{
+
+
+    public class StorageLoading
+    {
+        //Load method uses UserData Class as its data structure and it can be called to "Load" data from encrypted .Json files.
+        public static DataStorage Load(string user)
+
+        {
+            
+
+            //PATH TO USER FILES: repos\jsontestbench\jsontestbench\bin\Debug\net9.0\Users
+            string directoryPath = @AppDomain.CurrentDomain.BaseDirectory;
+            string mainFolder = @$"{directoryPath}\Users";
+            string newSubfolderName = user; // Define folder name from the given username
+            string newFolderPath = Path.Combine(mainFolder, newSubfolderName);
+
+
+
+
+            //Pull and decrypt the encrypted file, put it into the console.           
+            string encryptedUserFile = File.ReadAllText(@$"{mainFolder}\{user}\datastorage.json");  
+            DataStorage StringifiedUserStorage = JsonSerializer.Deserialize<DataStorage>(encryptedUserFile);
+            
+
+            //Returns UserData Class in Json deserialized Class form, aka. normal Class form where data can be used easily in C# functions.
+            return StringifiedUserStorage;
+
+        }
+
+          
+        }
+    }
+
+
+
+
+
+
+
+
+
+
