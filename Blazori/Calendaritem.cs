9a9e83a31a9da3ad68c02ed5e5dda8c01b45d5e1
Sakari Helander <sakari.helander@student.hamk.fi>
2025-10-15 13:19:19 +0300

    Added a chance to add and remove events to the calendar

--- Blazori/Calendaritem.cs
+++ Blazori/Calendaritem.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace ObjectOriented_Template
 {
-    public record CalendarItem(DateTime Date, string Name);
-  
+    //public record CalendarItem(DateTime Date, string Name);
+    public class CalendarItem
+    {
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+
+        public CalendarItem(DateTime date, string name)
+        {
+            Date = date;
+            Name = name;
+        }
+    }
 }
